3b68c1a4189054e135902ab2a56841b5b502ca43
Giorgos Logiotatidis <seadog@sealabs.net>
2011-09-26 15:17:24 +0300

    timezone fix

--- calendar_generator.py
+++ calendar_generator.py
@@ -11,7 +11,7 @@ from datetime import datetime, timedelta
 import googl
 
 QUERY = "http://hackerspace.gr/api.php?action=ask&q=[[Category:Events]]" \
-        "&format=json&po=location|Start%20date"
+        "&format=json&po=location|Start%20date|End%20date"
 
 HEADER = string.join(("BEGIN:VCALENDAR",
                      "PRODID:-//Hackerspace.gr Events//hackerspace.gr//",
@@ -49,9 +49,18 @@ def main():
             start_date = datetime.strptime(item['properties']['start_date'],
                                            "%Y-%m-%d %H:%M:%S"
                                            )
+
         except ValueError:
             # cannot parse date, move to next time
             continue
+
+        try:
+            end_date = datetime.strptime(item['properties']['end_date'],
+                                         "%Y-%m-%d %H:%M:%S"
+                                         )
+        except ValueError:
+            # cannot parse end date, make it equal to start_date + 1
+            end_date = start_date + timedelta(hour=1)
 
         try:
             uri = item['uri'][43:].encode("utf-8")
@@ -66,18 +75,25 @@ def main():
             (
                 "BEGIN:VEVENT",
                 "UID:%s@hsgr" % item['title'].encode('utf-8').replace(' ', '_'),
-                "DTSTAMP:%04d%02d%02dT%02d%02d00" % (start_date.year,
-                                                     start_date.month,
-                                                     start_date.day,
-                                                     start_date.hour,
-                                                     start_date.minute),
+                "DTSTAMP;TZID=Europe/Athens:%04d%02d%02dT%02d%02d00" % (
+                    start_date.year,
+                    start_date.month,
+                    start_date.day,
+                    start_date.hour,
+                    start_date.minute),
                 "ORGANIZER;CN=Hackerspace:MAILTO:mail@hackerspace.gr",
-                "DTSTART:%04d%02d%02dT%02d%02d00" % (start_date.year,
-                                                     start_date.month,
-                                                     start_date.day,
-                                                     start_date.hour,
-                                                     start_date.minute),
-
+                "DTSTART;TZID=Europe/Athens:%04d%02d%02dT%02d%02d00" % (
+                    start_date.year,
+                    start_date.month,
+                    start_date.day,
+                    start_date.hour,
+                    start_date.minute),
+                "DTEND;TZID=Europe/Athens:%04d%02d%02dT%02d%02d00" % (
+                    end_date.year,
+                    end_date.month,
+                    end_date.day,
+                    end_date.hour,
+                    end_date.minute),
                 "SUMMARY:%s" % unescape(item['title']).encode("utf-8"),
                 "DESCRIPTION:%s" % (url),
                 "END:VEVENT",
